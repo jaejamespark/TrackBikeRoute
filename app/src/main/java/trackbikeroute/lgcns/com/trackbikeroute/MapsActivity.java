2bea9f6fcd0696cd6af3f87e44a8b5cbcafb1ce1
Jae Park <jaejamespark@lge.com>
2016-05-10 23:47:11 -0700

    current location in progress

--- app/src/main/java/trackbikeroute/lgcns/com/trackbikeroute/MapsActivity.java
+++ app/src/main/java/trackbikeroute/lgcns/com/trackbikeroute/MapsActivity.java
@@ -1,8 +1,14 @@
 package trackbikeroute.lgcns.com.trackbikeroute;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -22,7 +28,14 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-    }
+
+        CurrentLocation mCurrentLocation = new CurrentLocation();
+        mCurrentLocation.getCurrentLocation();
+
+        }
+
+
+
 
 
     /**
@@ -43,4 +56,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
+
+
+
 }
